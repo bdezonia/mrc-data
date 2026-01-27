e5c8dfb1533ca1b071b75dfc513af0766e0140f5
Barry DeZonia <bdezonia@wisc.edu>
2026-01-27 14:24:40 -0600

    incorporate "nice" command into runit.sh
    crazy folks can remove it if they want.
    
    also expand rxc coverage up to 20x30. That will likely never finish
    but one can hope.

--- code/runit.sh
+++ code/runit.sh
@@ -1,5 +1,5 @@
-java -cp .:fastutil-8.5.18.jar DPScheduler \
-  --rMax 12 \
-  --cMax 19 \
+nice -n 5 java -cp .:fastutil-8.5.18.jar DPScheduler \
+  --rMax 20 \
+  --cMax 30 \
   --threads 2 \
   --out ../data
