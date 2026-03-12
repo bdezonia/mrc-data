97616070df4444b21a3102782aa8a4f5d8c41c22
Barry DeZonia <bdezonia@wisc.edu>
2026-03-12 13:09:54 -0500

    tweak runit.sh
    heap size code totally untested

--- code/runit.sh
+++ code/runit.sh
@@ -1,5 +1,5 @@
-nice -n 10 java -cp .:fastutil-8.5.18.jar DPScheduler \
+nice -n 10 java -Xmx8g -cp .:fastutil-8.5.18.jar DPScheduler \
   --rMax 30 \
   --cMax 30 \
-  --threads 2 \
+  --threads 1 \
   --out ../data
